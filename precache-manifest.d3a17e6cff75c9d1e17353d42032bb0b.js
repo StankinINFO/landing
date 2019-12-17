fa6bac56df96c0b24693ba6444cba520459b38e7
u17491330 <Kolyachenko.M.A@sberbank.ru>
2019-12-17 14:41:12 +0300

    Updates

---
+++ precache-manifest.d3a17e6cff75c9d1e17353d42032bb0b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b4afa20e52e0dc51cdc8e4684cf4eb",
+    "url": "/stankin-info/index.html"
+  },
+  {
+    "revision": "43586d783163b9da0c54",
+    "url": "/stankin-info/static/css/2.c726b981.chunk.css"
+  },
+  {
+    "revision": "86fece15d54fc4f4009e",
+    "url": "/stankin-info/static/css/main.0f9d1594.chunk.css"
+  },
+  {
+    "revision": "43586d783163b9da0c54",
+    "url": "/stankin-info/static/js/2.15faea48.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/stankin-info/static/js/2.15faea48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86fece15d54fc4f4009e",
+    "url": "/stankin-info/static/js/main.5e1488a1.chunk.js"
+  },
+  {
+    "revision": "46dd4751ea55af250df6",
+    "url": "/stankin-info/static/js/runtime-main.e55e4234.js"
+  },
+  {
+    "revision": "3f853e844bf6e40144a28f27d6c2b375",
+    "url": "/stankin-info/static/media/app-store-badge.3f853e84.svg"
+  },
+  {
+    "revision": "98502513efa1c2d90ddee7a54133aa3c",
+    "url": "/stankin-info/static/media/calendarColored.98502513.svg"
+  },
+  {
+    "revision": "ed4d79156515ddd2fc99d71c157fab6a",
+    "url": "/stankin-info/static/media/google-play-badge.ed4d7915.svg"
+  },
+  {
+    "revision": "0d881ca6177fd4e84aa54ded8370b665",
+    "url": "/stankin-info/static/media/hand2.0d881ca6.png"
+  },
+  {
+    "revision": "030f450076c58277dfca90c1bd2ae461",
+    "url": "/stankin-info/static/media/information.030f4500.svg"
+  },
+  {
+    "revision": "b10e7af8eb27325e9beeaab129c0c7d6",
+    "url": "/stankin-info/static/media/journal.b10e7af8.svg"
+  },
+  {
+    "revision": "2e1478b102f91afb670ea505d6b56ba1",
+    "url": "/stankin-info/static/media/modulesDisplay.2e1478b1.png"
+  },
+  {
+    "revision": "d6c7d9ecb219233153f9a8e4388e36fa",
+    "url": "/stankin-info/static/media/newIsometric.d6c7d9ec.svg"
+  },
+  {
+    "revision": "611d86c90ead2fbd4dcc3f916e906967",
+    "url": "/stankin-info/static/media/newIsometricColored.611d86c9.svg"
+  },
+  {
+    "revision": "7b92cd7fb260391529761bf541634404",
+    "url": "/stankin-info/static/media/newsDisplay.7b92cd7f.png"
+  },
+  {
+    "revision": "faf37b5e689775fea686d832203aa91e",
+    "url": "/stankin-info/static/media/phoneFrame.faf37b5e.svg"
+  },
+  {
+    "revision": "345a86d56c68c42b6c0d4df670934a2b",
+    "url": "/stankin-info/static/media/stankinLogo.345a86d5.png"
+  },
+  {
+    "revision": "13759ec5c7ad4a97a25ceb28707c6d67",
+    "url": "/stankin-info/static/media/stankinWhiteLogo.13759ec5.png"
+  }
+]);
