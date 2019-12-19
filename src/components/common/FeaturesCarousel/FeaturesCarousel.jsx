a2c1b27935b5c34d72a6636a9af8d98225403793
u17491330 <Kolyachenko.M.A@sberbank.ru>
2019-12-19 17:22:23 +0300

    Add swipe handle method on slider icons

--- src/components/common/FeaturesCarousel/FeaturesCarousel.jsx
+++ src/components/common/FeaturesCarousel/FeaturesCarousel.jsx
@@ -8,17 +8,9 @@ export default class FeaturesCarousel extends Component {
     constructor(props) {
         super(props)
         this.sliderIcon = React.createRef()
-    }
-
-    handleIconSelect = (id) => {
-        const { onSelectHandle, slides } = this.props
-
-        const currentSlideNumb = Number(id)
-        const currentSlideName = slides[currentSlideNumb].name
-
-        this.slider.current.goTo(currentSlideNumb)
-
-        onSelectHandle(currentSlideName)
+        this.state = {
+            touchStartX: null,
+        }
     }
 
     handleSlideBeforeChange = () => {
@@ -30,6 +22,21 @@ export default class FeaturesCarousel extends Component {
         this.sliderIcon.current.className = 'sliderIcon'
     }
 
+    handleOnTouchStart = (e) => {
+        this.setState({
+            touchStartX: e.screenX,
+        })
+    }
+
+    handleOnTouchEnd = (e) => {
+        const { touchStartX } = this.state
+        if (e.screenX < touchStartX) {
+            this.slider.current.prev()
+        } else if (e.screenX > touchStartX) {
+            this.slider.current.next()
+        }
+    }
+
     render() {
 
         const { slides, current } = this.props
@@ -38,7 +45,13 @@ export default class FeaturesCarousel extends Component {
         return (
             <div>
                 <div className="sliderIconContainer">
-                    <img className="sliderIcon" ref={ this.sliderIcon } src={currentSlide.icon} />
+                    <img
+                        className="sliderIcon"
+                        ref={ this.sliderIcon }
+                        src={currentSlide.icon}
+                        onTouchStart={ this.handleOnTouchStart }
+                        onTouchEnd={ this.handleOnTouchEnd }
+                    />
                 </div>
                 <Carousel
                     dots={true}
